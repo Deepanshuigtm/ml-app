c7d2339c6a539fa74c2b8681c03435acccf3f11e
your name <deepanshu@Deepanshus-MacBook-Air.local>
2024-04-25 23:23:22 +0530

    fixes api key err

--- frontend/src/components/ImageUploader.tsx
+++ frontend/src/components/ImageUploader.tsx
@@ -2,9 +2,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
 
-// const apiKey = "sk-proj-srNsiYtDmxRHKpBEZTRtT3BlbkFJBCg4bhuSfS7JCKRZ6xRz";
-// const apiKey2 = "sk-proj-oQdfDBRVBVjR1KBoDio2T3BlbkFJI38CN3SpCvTeT8pmq0JF";
-
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
 
 
@@ -17,24 +14,6 @@ const ImageUploader = () => {
 
     const ImageGenerate = async (e) => {
       e.preventDefault();
-
-      try {
-        const apiKey = "sk-proj-tUOfYArbsZCbkmr41X8GT3BlbkFJ5uk53crwXlMEdo9T20Kc"; // Replace "YOUR_API_KEY" with your actual API key
-        const apiText = await axios.post("https://api.openai.com/v1/images/generations", {
-          "prompt": "picture of blue cat in sea",
-          "n": 4,
-          "size": "1024x1024"
-        }, {
-          headers: {
-            "Authorization": `Bearer ${apiKey}`,
-            "Content-Type": "application/json"
-          }
-        });
-        console.log(apiText);
-      } catch (error) {
-        console.error(error);
-      }
-      
     }
 
   const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
